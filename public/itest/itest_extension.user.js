aef21ec65b6dd8cd0d3e875d87ef57c70b3c8879
Pham Doan Hieu <hieupd@rikkeisoft.com>
2021-08-10 01:38:19 +0700

    test release version update

--- public/itest/itest_extension.user.js
+++ public/itest/itest_extension.user.js
@@ -1,19 +1,34 @@
 // ==UserScript==
-// @name        itest support
+// @name        itest support extension
 // @description Fetch correct question and answer
-// @downloadURL  https://anonymouseykh.github.io/public/itest/itest_support.user.js
-// @updateURL  https://anonymouseykh.github.io/public/itest/itest_support.meta.js
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_openInTab
 // @grant       GM_registerMenuCommand
 // @include     http://itest.actvn.edu.vn/*
 // @include     http://anonymouseykh.github.io/*
-// @version     1.0.6
+// @version     1.0.0
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @run-at      document-end
 // ==/UserScript==
+var host = "https://anonymouseykh.github.io/public/itest"
+function getVersion() {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            method: "GET",
+            url: host + "/version",
+        }).then((data) => {
+            resolve(data);
+        }).fail((error) => {
+            reject(error);
+        });
+    });
+}
+async  function addScript(){
+    var version  = await getVersion();
+    var jq = document.createElement('script');
+    jq.src = "https://anonymouseykh.github.io/public/itest/itest_support_"+version+".js";
+    document.getElementsByTagName('head')[0].appendChild(jq);
+}
 
-var jq = document.createElement('script');
-jq.src = "https://anonymouseykh.github.io/public/itest/itest_support.user.js";
-document.getElementsByTagName('head')[0].appendChild(jq);
+addScript();
