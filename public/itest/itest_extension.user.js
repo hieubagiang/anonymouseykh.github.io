24bcf858b74733e56d152485704901b3d8827d0b
anonymouseykh <88678327+anonymouseykh@users.noreply.github.com>
2021-08-10 00:10:25 +0700

    Create itest_extension.user.js

---
+++ public/itest/itest_extension.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        itest support
+// @description Fetch correct question and answer
+// @downloadURL  https://anonymouseykh.github.io/public/itest/itest_support.user.js
+// @updateURL  https://anonymouseykh.github.io/public/itest/itest_support.meta.js
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_openInTab
+// @grant       GM_registerMenuCommand
+// @include     http://itest.actvn.edu.vn/*
+// @include     http://anonymouseykh.github.io/*
+// @version     1.0.6
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// @run-at      document-end
+// ==/UserScript==
+
+var jq = document.createElement('script');
+jq.src = "https://anonymouseykh.github.io/public/itest/itest_support.user.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
