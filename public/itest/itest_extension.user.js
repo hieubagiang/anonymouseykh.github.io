6933e9b2ce3eb40320a56ef6983c259e22597bea
Pham Doan Hieu <hieupd@rikkeisoft.com>
2021-08-10 02:15:26 +0700

    update to 1.0.6

--- public/itest/itest_extension.user.js
+++ public/itest/itest_extension.user.js
@@ -11,24 +11,27 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @run-at      document-end
 // ==/UserScript==
-var host = "https://anonymouseykh.github.io/public/itest"
-function getVersion() {
-    return new Promise((resolve, reject) => {
-        $.ajax({
-            method: "GET",
-            url: host + "/version",
-        }).then((data) => {
-            resolve(data);
-        }).fail((error) => {
-            reject(error);
-        });
+var versionLink = "https://anonymouseykh.github.io/public/itest/version.json"
+function getHTML(url, callBack) {
+    return new Promise(function() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                return callBack(xhttp.responseText);
+            }
+        };
+        xhttp.open("GET", url, true);
+        xhttp.send();
+
     });
 }
-async  function addScript(){
-    var version  = await getVersion();
+function addScript(version){
     var jq = document.createElement('script');
-    jq.src = "https://anonymouseykh.github.io/public/itest/itest_support_"+version+".js";
+    jq.src = "https://anonymouseykh.github.io/public/itest/release/"+version+".js";
     document.getElementsByTagName('head')[0].appendChild(jq);
+
 }
 
-addScript();
+getHTML("https://anonymouseykh.github.io/public/itest/version.json",function(value){
+    addScript(value)
+})
